39754439d9623a7420047f1a61f51242a5b7ebb6
Cedric ZH <cedriczhchan@gmail.com>
2016-09-20 13:55:04 +0800

    Edit Todo to use defaultChecked attribute

--- src/js/components/Todo.js
+++ src/js/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Settings extends React.Component {
   render(){
     return <li>
-      <input type="checkbox" checked={this.props.checked }/>
+      <input type="checkbox" defaultChecked={this.props.checked }/>
       { `\u00a0${this.props.text}` }
     </li>;
   }
